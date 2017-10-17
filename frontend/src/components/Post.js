cb0590562f0a06222f234c8ec577cdc81daf14fc
Matthew Robertson <mattrobertson1414@gmail.com>
2017-10-17 09:12:02 -0600

    Made posts only visible from front page and not comments

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import * as ReadableAPI from '../ReadableAPI.js'
-import Comment from './Comment.js'
 import './Post.css'
 
 class Post extends Component {
